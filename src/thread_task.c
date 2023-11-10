67d88e0c0387a94b5bd1d9c31130eb25888f8932
Chris Klochek <cklochek@replay.io>
2023-11-10 15:58:40 -0500

    Okay, lets just assert

--- src/thread_task.c
+++ src/thread_task.c
@@ -469,7 +469,7 @@ static inline void delayed_fg_task(const Dav1dContext *const c,
 }
 
 void *dav1d_worker_task(void *data) {
-    RecordReplayPrint("dav1d_worker_task start");
+    RecordReplayAssert("dav1d_worker_task start");
     Dav1dTaskContext *const tc = data;
     const Dav1dContext *const c = tc->c;
     struct TaskThreadData *const ttd = tc->task_thread.ttd;
